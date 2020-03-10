cc73be84795405a5f3d143374307c44c4ffa3584
Rock Boynton <boyntonrl@msoe.edu>
2020-03-10 17:35:51 -0500

    Working all in main

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,6 +9,7 @@ Rock Boynton
 #include <sstream>
 #include <limits>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
@@ -44,40 +45,39 @@ int main(int argc, char * argv[]) {
                 istringstream sstream(line);
                 sstream >> word; // ignore "vertex"
                 sstream >> num;
-
-                cout << "   x" << i << ":" << num << " ";
+                cout << setw(5) << "x" << i << ":" << fixed << setprecision(6) <<  num;
                 max[0] = std::max(max[0], num);
                 min[0] = std::min(min[0], num);
 
                 sstream >> num;
-                cout << "   y" << i << ":" << num << " ";
+                cout << setw(5) << "y" << i << ":" << fixed << setprecision(6) <<  num;
                 max[1] = std::max(max[1], num);
                 min[1] = std::min(min[1], num);
 
                 sstream >> num;
-                cout << "   z" << i << ":" << num << endl;
+                cout << setw(5) << "z" << i << ":" << fixed << setprecision(6) << num << endl;
                 max[2] = std::max(max[2], num);
                 min[2] = std::min(min[2], num);
 
                 getline(filein, line);
             }
-
             cout << endl;
             facets++;
         }
     }
     cout << "Summary" << endl;
 
-    cout << "   Max" << endl;
-    cout << "       x" << max[0];
-    cout << "       y" << max[1];
-    cout << "       z" << max[2] << endl;
+    cout << "Number of Facets: " << facets << endl;
 
-    cout << "   Min" << endl;
-    cout << "       x" << min[0];
-    cout << "       y" << min[1];
-    cout << "       z" << min[2] << endl;
+    cout << setw(6) << "Max" << endl;
+    cout << setw(10) << "x: " << showpos << max[0];
+    cout << setw(10) << "y: " << showpos << max[1];
+    cout << setw(10) << "z: " << showpos << max[2] << endl;
 
+    cout << setw(6) << "Min" << endl;
+    cout << setw(10) <<  "x: " << showpos << min[0];
+    cout << setw(10) <<  "y: " << showpos << min[1];
+    cout << setw(10) <<  "z: " << showpos << min[2] << endl;
 
     filein.close();
     return 0;
