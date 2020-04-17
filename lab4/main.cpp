c4425baff255789959234526415ade582bad8649
Rock Boynton <boyntonrl@msoe.edu>
2020-04-17 15:02:38 -0500

    Add bullseye image to main

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,45 +4,63 @@
  
 int main(void)
 {
-	GraphicsContext* gc = new X11Context(800,600,GraphicsContext::BLACK);
- 
-	// gc->setColor(GraphicsContext::GREEN);
-	// gc->drawLine(100,100,500,200);
-	// gc->setColor(GraphicsContext::RED);
-	// gc->drawLine(100,100,200,800);
-	// gc->setColor(GraphicsContext::BLUE);
-	// gc->drawLine(100,500,500,200);
-	// // gc->setColor(GraphicsContext::YELLOW);
-	// // gc->drawLine(500,100,100,100);
-	// // gc->setColor(GraphicsContext::MAGENTA);
-	// // gc->drawCircle(300,300,200);
-	// sleep(5);
+	GraphicsContext* gc = new X11Context(800,600,GraphicsContext::WHITE);
  
 	// draw some lines
-	gc->setColor(GraphicsContext::GREEN);
-	gc->setPixel(10,10);
-	gc->setPixel(30,30);
-	gc->drawLine(100,100,100,500);
+	// gc->setColor(GraphicsContext::GREEN);
+	// gc->setPixel(10,10);
+	// gc->setPixel(30,30);
+	// gc->drawLine(100,100,100,500);
+	// gc->setColor(GraphicsContext::RED);
+	// gc->drawLine(100,500,500,500);
+	// gc->setColor(GraphicsContext::BLUE);
+	// gc->drawLine(500,500,500,100);
+	// gc->setColor(GraphicsContext::YELLOW);
+	// gc->drawLine(500,100,100,100);
+
+	// gc->setColor(GraphicsContext::MAGENTA);
+	// gc->drawCircle(300,300,200);
+	// sleep(5);
+
 	gc->setColor(GraphicsContext::RED);
-	gc->drawLine(100,500,500,500);
-	gc->setColor(GraphicsContext::BLUE);
-	gc->drawLine(500,500,500,100);
-	gc->setColor(GraphicsContext::YELLOW);
-	gc->drawLine(500,100,100,100);
+	for (int i = 0; i < 50; i++)
+	{
+		gc->drawCircle(400,300,250+ i);
+	}
 
-	gc->setColor(GraphicsContext::CYAN);
-	gc->drawLine(400,100, 100,500);
+	// for (int i = 0; i < 25; i++)
+	// {
+	// 	gc->drawCircle(400,300,200 + i);
+	// }
 
-	gc->setColor(GraphicsContext::GRAY);
-	gc->drawLine(400,500, 100,100);
+	for (int i = 0; i < 50; i++)
+	{
+		gc->drawCircle(400,300, 150 + i);
+	}
 
-	gc->setColor(GraphicsContext::WHITE);
-	gc->drawLine(300,300,441,441);
+	// for (int i = 0; i < 25; i++)
+	// {
+	// 	gc->drawCircle(400,300, 100 + i);
+	// }
+	
+	for (int i = 0; i < 50; i++)
+	{
+		gc->drawCircle(400,300,50 + i);
+	}
 
-	gc->setColor(GraphicsContext::MAGENTA);
-	gc->drawCircle(300,300,200);
-	sleep(5);
- 
+	gc->setColor(GraphicsContext::BLACK);
+	gc->drawLine(0,600,390,310);
+	gc->drawLine(0,0,390,290);
+	gc->drawLine(800,0,410,290);
+	gc->drawLine(800,600,410,310);
+
+	for (int i = 0; i < 5; i++)
+	{
+		gc->drawCircle(400,300,i);
+	}
+
+ 	sleep(5);
+
 	delete gc;
  
 	return 0;
