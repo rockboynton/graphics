bb2554a601829e003d97afd6fda105cdb166c41c
Rock Boynton <boyntonrl@msoe.edu>
2020-04-21 18:01:07 -0500

    Add triangle class

---
+++ lab5/triangle.cpp
@@ -0,0 +1,45 @@
+#include "triangle.h"
+
+
+Triangle::Triangle(int x0, int y0, int x1, int y1, int x2, int y2, unsigned int color): Shape(color)
+{
+    // TODO
+}
+
+Triangle::Triangle(const Triangle& from): Shape(from.color)
+{
+    // TODO
+}
+
+Triangle::~Triangle()
+{
+    // TODO
+}
+
+Triangle& Triangle::draw(GraphicsContext& gc)
+{
+    // TODO
+    return *this;
+}
+
+Shape* Triangle::clone()
+{
+    // TODO
+    return this;
+}
+
+Triangle& Triangle::operator=(const Shape& rhs)
+{
+    // TODO
+    return *this;
+}
+
+void Triangle::out(std::ostream& os)
+{
+    // TODO
+}
+
+void Triangle::in(std::istream& is)
+{
+    // TODO
+}
---
+++ lab5/triangle.h
@@ -0,0 +1,79 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <array>
+
+#include "shape.h"
+#include "matrix.h"
+
+class Triangle: public Shape
+{
+    public:
+        /**
+         * @brief Construct a new Triangle object
+         * 
+         * @param x0 - starting x 
+         * @param y0 - starting y
+         * @param x1 - middle x
+         * @param y1 - middle y
+         * @param x2 - ending x
+         * @param y2 - ending y
+         * @param color - color of the Triangle
+         */
+        Triangle(int x0, int y0, int x1, int y1, int x2, int y2, unsigned int color);
+
+        /**
+         * @brief Copy construct a new Triangle object from another Triangle
+         * 
+         * @param from - Triangle to copy 
+         */
+        Triangle(const Triangle& from);
+
+        /**
+         * @brief Destroy the Triangle object
+         * 
+         */
+        ~Triangle();
+
+        /**
+         * @brief Draw this Triangle in provided Graphics context.
+         * 
+         * @param gc - GraphicsContext to draw in
+         * @return Shape& - reference to this to allow chaining
+         */
+        Triangle& draw(GraphicsContext& gc);
+
+        /**
+         * @brief Copy this Triangle object and return a Shape pointer to it
+         * 
+         * @return Shape* - pointer containing clone of this
+         */
+        Shape* clone();
+
+        /**
+         * @brief Create a new Triangle just like rhs. 
+         * 
+         * @param rhs - Triangle which properties are copied
+         * @return Shape& - reference to this to allow chaining
+         */
+        Triangle& operator=(const Shape& rhs);
+
+        /**
+         * @brief Print Triangle properties to the output stream.
+         * 
+         * @param os - Output Stream reference (std::ostream&)
+         */
+        void out(std::ostream& os);
+
+        /**
+         * @brief Construct a Triangle object from input stream
+         * 
+         * @param is 
+         */
+        void in(std::istream& is);
+
+    private:
+        std::array<matrix*, 3> coordinates; // holds {start, middle, end}
+};
+
+#endif
