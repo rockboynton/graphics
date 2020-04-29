fdb7a95ccd4d3dc285a213bd3f1a08fe2baf1c29
Rock Boynton <boyntonrl@msoe.edu>
2020-04-29 00:41:51 -0500

    Implement line constructor

--- lab5/line.cpp
+++ lab5/line.cpp
@@ -1,8 +1,26 @@
 #include "line.h"
 
+
+/**
+ * @brief Construct a new Line object
+ * 
+ * @param x0 
+ * @param y0 
+ * @param x1 
+ * @param y1 
+ * @param color 
+ */
 Line::Line(int x0, int y0, int x1, int y1, unsigned int color): Shape(color)
 {
-    // TODO
+    int params[2][2] = {{x0, y0}, {x1, y1}};
+    int point = 0;
+    for (auto& coordinate : coordinates) {
+        coordinate = std::make_unique<matrix>(matrix::COORDINATE_ROWS , matrix::COORDINATE_COLS);
+        (*coordinate)[0][0] = params[point][0]; // x-coord
+        (*coordinate)[1][0] = params[point++][1]; // y-coord
+        (*coordinate)[2][0] = 0; // z-coord
+        (*coordinate)[3][0] = 1; // homogenous-coord
+    }
 }
 
 Line::Line(const Line& from): Shape(from.color)
